e6ab94ee730b90f395f71cb3d6c8e01cf2f16916
gunda manoharreddy <gundamanoharreddy15@gmail.com>
2026-05-26 11:37:10 +0530

    fix demo user password

--- backend/breathe/settings.py
+++ backend/breathe/settings.py
@@ -100,6 +100,7 @@ USE_I18N = True
 USE_TZ = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+APPEND_SLASH = True
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
